4d2ac56b9dcf0223896e68860dde8b58e8016025
matvisiv2 <matvisiv2@gmail.com>
2025-12-27 23:11:27 +0200

    f

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || `http://localhost:4444/api`,
+  // baseURL: process.env.REACT_APP_API_URL || "http://localhost:4444/api",
+  baseURL: process.env.REACT_APP_API_URL || "https://team1-taskboard-backend.onrender.com/api",
 });
 
 instance.interceptors.request.use((config) => {
